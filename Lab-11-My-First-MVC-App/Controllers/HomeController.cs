fb81dd4682a45e158e1c0d809b8a3d27244f1575
Tulip Pink <pinktulips@hotmail.com>
2019-01-23 11:19:48 -0800

    add

---
+++ Lab-11-My-First-MVC-App/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_11_My_First_MVC_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+           
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Index(int firstNumber, int secondNumber)
+        {
+            return RedirectToAction("Results", new { firstNumber, secondNumber });
+        }
+
+        public IActionResult Results(int firstNumber, int secondNumber)
+        {
+            //do what we want with the numbers
+
+            return View();
+        }
+    }
+}
--- Lab-11-My-First-MVC-App/Startup.cs
+++ Lab-11-My-First-MVC-App/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Lab_11_My_First_MVC_App  
+namespace Lab_11_My_First_MVC_App
 {
     public class Startup
     {
@@ -15,6 +15,7 @@ namespace Lab_11_My_First_MVC_App
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,18 +26,14 @@ namespace Lab_11_My_First_MVC_App
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc(route =>
-            route.MapRoute(
-                name: "default",
-                template: "{controller=Home/{action=Index}/{id?}"
-                ));
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.UseStaticFiles();
-;
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
         }
     }
 }
