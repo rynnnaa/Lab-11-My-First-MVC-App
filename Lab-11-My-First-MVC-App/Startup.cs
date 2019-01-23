d32e2d7cd42ebdf64c8c5b5deee4235af2af9446
Tulip Pink <pinktulips@hotmail.com>
2019-01-23 10:41:54 -0800

    initial commit

--- Lab-11-My-First-MVC-App/Startup.cs
+++ Lab-11-My-First-MVC-App/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Lab_11_My_First_MVC_App
+namespace Lab_11_My_First_MVC_App  
 {
     public class Startup
     {
